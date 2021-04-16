f5ed3a337d8e710a5f7525db0688d7b77865fe57
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-04-16 11:36:28 -0400

    Update

--- src/Millionaire/EasyRound.java
+++ src/Millionaire/EasyRound.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class EasyRound extends Round {
     // Constant
     private static final int NUM_QUESTIONS = 3;
-    //private static final int QUESTION_1_VALUE =
 
     public EasyRound(ArrayList<Question> questions) {
         super(questions);
