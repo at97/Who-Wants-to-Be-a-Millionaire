1a7dbd8ffddadb30a79f902633c38bc126866f46
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-04-16 23:39:05 -0400

    Fix bug that where 'l' was a valid input

--- src/Millionaire/Round.java
+++ src/Millionaire/Round.java
@@ -12,6 +12,7 @@ public abstract class Round {
     private static int roundNumber;
     private static int difficulty;
     private ArrayList<Question> questions;
+    private boolean usingLifeline;
 
     public Round(ArrayList<Question> questions) {
         roundNumber++;
@@ -38,7 +39,16 @@ public abstract class Round {
         Round.difficulty = difficulty;
     }
 
+    public boolean isUsingLifeline() {
+        return usingLifeline;
+    }
+
+    public void setUsingLifeline(boolean usingLifeline) {
+        this.usingLifeline = usingLifeline;
+    }
+
     public void showQuestion(int index) {
+        // Show question and it's associated answers
         System.out.println(questions.get(index).getQuestion());
         System.out.println("  A. " + questions.get(index).getAllAnswers().get(0));
         System.out.println("  B. " + questions.get(index).getAllAnswers().get(1));
@@ -51,9 +61,11 @@ public abstract class Round {
         if ((difficulty == EASY_MODE) || (difficulty == HARD_MODE && (roundNumber == 2 || roundNumber == 3))) {
             // Check for unused lifelines
             if (!Lifeline.isUsed5050Lifeline() || !Lifeline.isUsedAudienceLifeline() || !Lifeline.isUsedFriendLifeline()) {
+                // There are still unused lifelines
                 return true;
             }
         }
+        // There are no lifelines left or lifelines cannot be used for the present round
         return false;
     }
 
@@ -75,19 +87,35 @@ public abstract class Round {
         boolean valid = false;
         while (!valid) {
             try{
+                // Select lifeline
                 System.out.print("Please select an option: ");
                 int lifeline = MillionaireGameApp.read.nextInt();
                 if (lifeline == 1 && !Lifeline.isUsed5050Lifeline()) {
+                    // Select 50/50 lifeline
+                    usingLifeline = true;
                     FiftyFifty fiftyFiftyObj = new FiftyFifty();
                     Lifeline.setUsed5050Lifeline(true);
                     Question newQuestion = fiftyFiftyObj.useLifeline(question);
+                    // Hide incorrect answers
                     questions.get(index).setAllAnswers(newQuestion.getAllAnswers());
                     valid = true;
                 }
                 if (lifeline == 2 && !Lifeline.isUsedAudienceLifeline()) {
+                    // Ask the audience
+                    usingLifeline = true;
                     Audience audienceObj = new Audience();
                     Lifeline.setUsedAudienceLifeline(true);
                     Question newQuestion = audienceObj.useLifeline(question);
+                    valid = true;
+                }
+                if (lifeline == 3 && !Lifeline.isUsedFriendLifeline()) {
+                    // Phone a friend
+                    usingLifeline = true;
+                    System.out.print("Please enter the name of the friend you would like to call: ");
+                    String friendName = MillionaireGameApp.read.next();
+                    Friend friendObj = new Friend(friendName);
+                    Lifeline.setUsedFriendLifeline(true);
+                    Question newQuestion = friendObj.useLifeline(question);
                     valid = true;
                 }
             } catch (InputMismatchException e) {
@@ -101,16 +129,23 @@ public abstract class Round {
         boolean validInput = false;
         char answer = 0;
         while (!validInput) {
+            // Get user's answer
             System.out.print("Please select an answer: ");
             answer = MillionaireGameApp.read.next().charAt(0);
+            // Convert answer to lowercase
             answer = Character.toLowerCase(answer);
+            // Check if lifeline can be used in case 'l' is a valid input
             boolean lifelineEligible = checkLifelineEligibility();
-            if (!(answer == 'a' || answer == 'b' || answer == 'c' || answer == 'd' || (answer == 'l' && lifelineEligible))) {
+
+            if (!(answer == 'a' || answer == 'b' || answer == 'c' || answer == 'd' || (answer == 'l' && !usingLifeline && lifelineEligible))) {
+                // User enters an input other than 'a', 'b', 'c', 'd', or 'l' while lifelines are available and not being used
                 System.out.print("Error: not a valid option. ");
             } else {
+                // User enters valid input
                 validInput = true;
             }
         }
+        // Return user's answer
         return answer;
     }
 
@@ -118,12 +153,14 @@ public abstract class Round {
         boolean finalAnswer = false;
         char response;
         int integerAnswer = 0;
-        boolean lifelineCheck = checkLifelineEligibility();
         while (!finalAnswer) {
+            // Verify if the user's reponse if what they want
             System.out.print("Is '" + answer + "' your final answer? Enter 'y' or 'n': ");
             response = MillionaireGameApp.read.next().charAt(0);
+            // Prompt for yes or no
             response = Character.toLowerCase(response);
             if (response == 'y') {
+                // It is their final answer
                 finalAnswer = true;
                 // Match their answer to an integer to check if it's correct
                 switch (answer) {
@@ -141,6 +178,7 @@ public abstract class Round {
                         break;
                 }
             } else if (response == 'n') {
+                // Ask user for another response
                 answer = getAnswer();
             } else {
                 System.out.println("Error: not a valid option.");
@@ -152,6 +190,7 @@ public abstract class Round {
     public boolean checkAnswer(Question question, int answer) {
         boolean correctAnswer = false;
         try {
+            // Compare user's answer to answer from question
             if (answer == question.getAnswer()) {
                 correctAnswer = true;
             }
@@ -167,10 +206,13 @@ public abstract class Round {
         while (!valid) {
             System.out.print("Would you like to walk away? Enter 'y' or 'n': ");
             response = MillionaireGameApp.read.next().charAt(0);
+            // Prompt user if they want to walk away with their earnings
             response = Character.toLowerCase(response);
             if (response == 'y') {
+                // User walks away with winnings
                 valid = true;
             } else if (response == 'n') {
+                // User continues with game
                 return false;
             } else {
                 System.out.print("Error: not a valid option. ");
